238c2956a5247a55880e140a689f5736d644e146
saramotaha <saramohamedtaha909@gmail.com>
2025-07-18 23:54:12 +0300

    start with property category

---
+++ bnbClone-API/Controllers/PropertyCategoryController.cs
@@ -0,0 +1,52 @@
+﻿using bnbClone_API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace bnbClone_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyCategoryController : ControllerBase
+    {
+        private readonly IPropertyCategoryRepo propertyCategoryRepo;
+
+
+
+        public PropertyCategoryController(IPropertyCategoryRepo propertyCategoryRepo)
+        {
+            this.propertyCategoryRepo = propertyCategoryRepo;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+
+            return Ok(await propertyCategoryRepo.GetAllAsync());
+
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAllCategoryById(int id)
+        {
+
+            return Ok(await propertyCategoryRepo.GetByIdAsync(id));
+
+        }
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
---
+++ bnbClone-API/Repositories/Impelementations/PropertyCategoryRepo.cs
@@ -0,0 +1,14 @@
+﻿using bnbClone_API.Data;
+using bnbClone_API.Models;
+using bnbClone_API.Repositories.Interfaces;
+
+namespace bnbClone_API.Repositories.Impelementations
+{
+    public class PropertyCategoryRepo: GenericRepo<PropertyCategory> , IPropertyCategoryRepo
+    {
+
+        public PropertyCategoryRepo(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
