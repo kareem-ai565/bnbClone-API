fd66f746efa5260776c20d5b4acc600410f9fca6
saramotaha <saramohamedtaha909@gmail.com>
2025-07-18 21:36:52 +0300

    complete work on Amenity controller

--- bnbClone-API/Controllers/AmenityController.cs
+++ bnbClone-API/Controllers/AmenityController.cs
@@ -1,4 +1,6 @@
-﻿using bnbClone_API.Repositories.Interfaces;
+﻿using bnbClone_API.DTOs;
+using bnbClone_API.Models;
+using bnbClone_API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -17,6 +19,7 @@ namespace bnbClone_API.Controllers
         }
 
 
+
         [HttpGet]
         public async Task<IActionResult> GetAllAmenities()
         {
@@ -24,6 +27,69 @@ namespace bnbClone_API.Controllers
           return  Ok( await amenityRepo.GetAllAsync());
 
         }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAmenityById(int id)
+        {
+
+            return Ok(await amenityRepo.GetByIdAsync(id));
+
+        }
+
+
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAmenity(int id)
+        {
+
+            return Ok(await amenityRepo.DeleteAsync(id));
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddAmenity([FromForm] AmenityDTO amenity)
+        {
+
+            var FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images");
+
+            Amenity amenity1 = new Amenity()
+            {
+                Id = amenity.Id,
+                Name = amenity.Name,
+                Category = amenity.Category,
+                IconUrl = amenity.IconUrl,
+            };
+
+
+            await amenityRepo.AddAsync(amenity1);
+
+            return Ok(amenity);
+
+        }
+
+
+
+
+
+
+        //[HttpPut]
+        //public IActionResult UpdateAmenity()
+        //{
+
+        //}
+
+
+
+
+
+
+
 
 
     }
---
+++ bnbClone-API/DTOs/AmenityDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bnbClone_API.DTOs
+{
+    public class AmenityDTO
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        
+        public string? Category { get; set; }
+        
+        public IFormFile? IconUrl { get; set; }
+
+    }
+}
