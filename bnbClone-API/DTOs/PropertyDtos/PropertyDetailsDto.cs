1c6d9d25d1edf7890ec0d8a6751c178598d2c5e6
saramotaha <saramohamedtaha909@gmail.com>
2025-07-23 16:03:56 +0300

    add prop

--- bnbClone-API/DTOs/PropertyDtos/PropertyDetailsDto.cs
+++ bnbClone-API/DTOs/PropertyDtos/PropertyDetailsDto.cs
@@ -6,6 +6,7 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public string City { get; set; }
+        public string Country { get; set; }
         public decimal PricePerNight { get; set; }
         public int MaxGuests { get; set; }
         public int NumOfBedrooms { get; set; }
